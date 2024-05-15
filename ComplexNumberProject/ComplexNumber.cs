910466103772e09c3973ada3a0636f6614ef7c2e
Alex <alexey2023.com>
2024-05-15 13:06:33 +0300

    Добавьте файлы проекта.

---
+++ ComplexNumberProject/ComplexNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class ComplexNumber
+{
+    public double Real { get; set; }
+    public double Imaginary { get; set; }
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    // Перегрузка оператора сложения
+    public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+    {
+        return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+    }
+
+    // Перегрузка оператора вычитания
+    public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+    {
+        return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+    }
+
+    // Перегрузка оператора умножения
+    public static ComplexNumber operator *(ComplexNumber c1, ComplexNumber c2)
+    {
+        double realPart = c1.Real * c2.Real - c1.Imaginary * c2.Imaginary;
+        double imaginaryPart = c1.Real * c2.Imaginary + c2.Real * c1.Imaginary;
+        return new ComplexNumber(realPart, imaginaryPart);
+    }
+
+    // Перегрузка оператора деления
+    public static ComplexNumber operator /(ComplexNumber c1, ComplexNumber c2)
+    {
+        double denominator = c2.Real * c2.Real + c2.Imaginary * c2.Imaginary;
+        double realPart = (c1.Real * c2.Real + c1.Imaginary * c2.Imaginary) / denominator;
+        double imaginaryPart = (c1.Imaginary * c2.Real - c1.Real * c2.Imaginary) / denominator;
+        return new ComplexNumber(realPart, imaginaryPart);
+    }
+
+    // Перегрузка оператора сравнения 
+    public static bool operator ==(ComplexNumber c1, ComplexNumber c2)
+    {
+        return c1.Real == c2.Real && c1.Imaginary == c2.Imaginary;
+    }
+
+    public static bool operator !=(ComplexNumber c1, ComplexNumber c2)
+    {
+        return !(c1 == c2);
+    }
+
+    // Метод преобразования в строку
+    public override string ToString()
+    {
+        return $"{Real} + {Imaginary}i";
+    }
+
+    // Статический метод для получения комплексного числа из строки
+    public static ComplexNumber Parse(string input)
+    {
+        string[] parts = input.Split('+', 'i');
+        double real = double.Parse(parts[0]);
+        double imaginary = double.Parse(parts[1]);
+        return new ComplexNumber(real, imaginary);
+    }
+}
+
---
+++ ComplexNumberProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите комплексное число в формате a+bi:");
+        Console.Write("действительная часть a = ");
+        double realPart = double.Parse(Console.ReadLine());
+
+        Console.Write("мнимая часть b = ");
+        double imaginaryPart = double.Parse(Console.ReadLine());
+
+        // Создание комплексного числа
+        ComplexNumber complex1 = new ComplexNumber(realPart, imaginaryPart);
+
+        Console.WriteLine("Введите второе комплексное число в формате a+bi:");
+        Console.Write("действительная часть a = ");
+        realPart = double.Parse(Console.ReadLine());
+
+        Console.Write("мнимая часть b = ");
+        imaginaryPart = double.Parse(Console.ReadLine());
+
+        // Создание второго комплексного числа
+        ComplexNumber complex2 = new ComplexNumber(realPart, imaginaryPart);
+
+        // Сложение комплексных чисел
+        ComplexNumber sum = complex1 + complex2;
+        Console.WriteLine($"Сумма: {sum}");
+
+        // Вычитание комплексных чисел
+        ComplexNumber difference = complex1 - complex2;
+        Console.WriteLine($"Разность: {difference}");
+
+        // Умножение комплексных чисел
+        ComplexNumber product = complex1 * complex2;
+        Console.WriteLine($"Произведение: {product}");
+
+        // Деление комплексных чисел
+        ComplexNumber quotient = complex1 / complex2;
+        Console.WriteLine($"Частное: {quotient}");
+    }
+}
